df1b409ab97907beb7b9612508b7e4ca630ceba6
Varneon <me@varneon.com>
2023-02-26 02:18:35 -0800

    Add PlayerScaleConstraint

---
+++ Packages/com.varneon.vudon.player-scale-utility/Runtime/Scripts/PlayerScaleConstraint.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.PlayerScaleUtility
+{
+    /// <summary>
+    /// Component for describing a transform that should always be scaled according to the player's scale
+    /// </summary>
+    public class PlayerScaleConstraint : MonoBehaviour { }
+}
