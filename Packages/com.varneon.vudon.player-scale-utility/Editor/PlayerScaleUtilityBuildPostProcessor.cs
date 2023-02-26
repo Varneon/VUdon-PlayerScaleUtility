ce871250a84436913d82dfae7a3cf2bff8b190a9
Varneon <me@varneon.com>
2023-02-26 03:43:39 -0800

    Add build postprocessor

---
+++ Packages/com.varneon.vudon.player-scale-utility/Editor/PlayerScaleUtilityBuildPostProcessor.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using System.Linq;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using Varneon.VUdon.PlayerScaleUtility.Abstract;
+
+namespace Varneon.VUdon.PlayerScaleUtility.Editor
+{
+    /// <summary>
+    /// Build postprocessor for PlayerScaleUtility
+    /// </summary>
+    public static class PlayerScaleUtilityBuildPostProcessor
+    {
+        /// <summary>
+        /// Processes the PlayerScaleUtility on build
+        /// </summary>
+        [UsedImplicitly]
+        [PostProcessScene(-1)]
+        private static void PostProcessPlayerScaleUtility()
+        {
+            // Get the PlayerScaleUtility from the scene
+            PlayerScaleUtility playerScaleUtility = Object.FindObjectOfType<PlayerScaleUtility>();
+
+            // Find all objects with PlayerScaleConstraint
+            Transform[] playerScaleConstrainedTransforms = Resources.FindObjectsOfTypeAll<PlayerScaleConstraint>().Where(c => c.gameObject.scene.IsValid()).Select(c => c.transform).ToArray();
+
+            // Assign constrained transforms to the utility
+            playerScaleUtility.constrainedTransforms = playerScaleConstrainedTransforms;
+
+            // Find all PlayerScaleCallbackReceivers
+            PlayerScaleCallbackReceiver[] playerScaleCallbackReceivers = Resources.FindObjectsOfTypeAll<PlayerScaleCallbackReceiver>().Where(r => r.gameObject.scene.IsValid()).ToArray();
+
+            // Assign callback receivers to the utility
+            playerScaleUtility.callbackReceivers = playerScaleCallbackReceivers;
+        }
+    }
+}
