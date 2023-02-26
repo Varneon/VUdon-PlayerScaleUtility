7a13ea591af6149dbb9edbc097ff53fd95ca5335
Varneon <me@varneon.com>
2023-02-26 02:52:01 -0800

    Add abstract player scale callback receiver

---
+++ Packages/com.varneon.vudon.player-scale-utility/Runtime/Udon Programs/Abstract/PlayerScaleCallbackReceiver.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+
+namespace Varneon.VUdon.PlayerScaleUtility.Abstract
+{
+    /// <summary>
+    /// Callback receiver for player's scale changes
+    /// </summary>
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public abstract class PlayerScaleCallbackReceiver : UdonSharpBehaviour
+    {
+        /// <summary>
+        /// Gets called when the player's scale changes
+        /// </summary>
+        /// <param name="newPlayerScale">Player's new scale</param>
+        public abstract void OnPlayerScaleChanged(float newPlayerScale);
+    }
+}
