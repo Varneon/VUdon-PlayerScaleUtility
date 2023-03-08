dd4fba29b0664c2a41d7061bd106e5cdc27e9dd2
Varneon <me@varneon.com>
2023-03-08 03:59:29 -0800

    Add info box to PlayerScaleConstraint inspector

---
+++ Packages/com.varneon.vudon.player-scale-utility/Editor/PlayerScaleConstraintEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Varneon.VUdon.PlayerScaleUtility.Editor
+{
+    [CustomEditor(typeof(PlayerScaleConstraint))]
+    public class PlayerScaleConstraintEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.HelpBox("This object's local scale will be linked to local player's scale.", MessageType.Info);
+        }
+    }
+}
