e95ce9f52f93c7bbe1f2601bbc6d183cf0193bef
Varneon <me@varneon.com>
2023-05-11 05:21:43 -0700

    Add AddComponent menu item for constraint

--- Packages/com.varneon.vudon.player-scale-utility/Runtime/Scripts/PlayerScaleConstraint.cs
+++ Packages/com.varneon.vudon.player-scale-utility/Runtime/Scripts/PlayerScaleConstraint.cs
@@ -5,5 +5,6 @@ namespace Varneon.VUdon.PlayerScaleUtility
     /// <summary>
     /// Component for describing a transform that should always be scaled according to the player's scale
     /// </summary>
+    [AddComponentMenu("VUdon/Player Scale Utility/Player Scale Constraint")]
     public class PlayerScaleConstraint : MonoBehaviour { }
 }
